db3b6738d2addd74d8d8875d770714b134f8ca23
NongFirstTH <TanabodeeChannel@gmail.com>
2022-12-27 16:03:29 +0700

    OK

--- lab10_1.cpp
+++ lab10_1.cpp
@@ -2,11 +2,14 @@
 #include<iomanip> //For using setw(), setprecision(), ...
 using namespace std;
 
-int main(){	
+int main(){
+	double a,b,c;
 	cout << "Enter initial loan: ";
+	cin >> a;
 	cout << "Enter interest rate per year (%): ";
+	cin >> b;
 	cout << "Enter amount you can pay per year: ";
-
+	cin >> c;
 	//use 'setw' to set width of table and 'left' to set left-alignment
 	//you can change input argument of 'setw()' to see the effect
 	//Try to change from 'left' to 'right' and see the effect
@@ -20,14 +23,25 @@ int main(){
 	
 	//use 'fixed' and 'setprecision' to fix the number of decimal digits for displaying
 	//you can change input argument of 'setprecision()' to see the effect
+	double pre=a,inter,pay=c,total,newb=a;
+	int i = 1;
+	while(newb>0){
 	cout << fixed << setprecision(2); 
-	cout << setw(13) << left << 1; 
-	cout << setw(13) << left << 1000.0;
-	cout << setw(13) << left << 50.0;
-	cout << setw(13) << left << 1050.0;
-	cout << setw(13) << left << 100.0;
-	cout << setw(13) << left << 950.0;
-	cout << "\n";	
-	
+	cout << setw(13) << left << i; 
+	cout << setw(13) << left << pre;
+	inter = pre*(b/100);
+	cout << setw(13) << left << inter;
+	total = pre + inter;
+	cout << setw(13) << left << total;
+	if(total<pay){
+		pay = total;
+	}
+	cout << setw(13) << left << pay;
+	newb = total-pay;
+	cout << setw(13) << left << newb;
+	cout << "\n";
+	pre = newb;	
+	i++;
+	}
 	return 0;
 }
--- lab10_2.cpp
+++ lab10_2.cpp
@@ -1,7 +1,15 @@
 #include<iostream>
 using namespace std;
 
-//Write the function printO() here
+void printO(int M,int N){
+    if(M<=0 or N<=0){cout << "Invalid input";}else{
+   for(int i = 0;i <M;i++){
+   for(int j = 0;j < N;j++){
+    cout << 'O';
+   }
+   cout << "\n";
+   }}
+}
 
 int main(){
 	
